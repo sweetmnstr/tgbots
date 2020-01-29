b7ea64c27094eff355ed74104078d5d157216fee
Sweetmnstr <progran98@gmail.com>
2020-01-29 02:02:52 +0200

    ававывы

--- cmp.py
+++ cmp.py
@@ -1,10 +1,29 @@
 import telebot
 import config
+import random
+
+from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(context_types = ["text"] )
-def exo(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands = ["start"])
+def wlcm(message):
 
-bot.polling(non_stop = True)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item = types.KeyboardButton("Random cat sticker")
+
+    markup.add(item)
+
+    bot.send_message(message.chat.id, "Приветик, {0.first_name}!\nУ меня один ссаный вопрос, ты шо, ебанутый, чтобы сюда заходить?".format(message.from_user, bot.get_me()), parse_mode = "html", reply_markup = markup)
+
+@bot.message_handler(content_types = ["text"])
+def randcatsticker(message):
+    if message.chat.type == "private":
+        if message.text == "Random cat sticker":
+            sti = open('static/s'+ str(random.randint(0 , 20)) + '.webp', 'rb')
+            bot.send_sticker(message.chat.id, sti)
+
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop = True)
